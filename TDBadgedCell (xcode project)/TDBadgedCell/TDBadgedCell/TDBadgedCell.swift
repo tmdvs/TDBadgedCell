d1108c50dbbdef7dc4378d44525f748ad2d43560
Tim Davies <timdavies@me.com>
2016-09-07 13:01:27 +0100

    GitIngore update

---
+++ TDBadgedCell (xcode project)/TDBadgedCell/TDBadgedCell/TDBadgedCell.swift
@@ -0,0 +1,13 @@
+//
+//  TDBadgedCell.swift
+//  TDBadgedCell
+//
+//  Created by Tim Davies on 07/09/2016.
+//  Copyright © 2016 Tim Davies. All rights reserved.
+//
+
+import Cocoa
+
+class TDBadgedCell: UITableViewCell {
+
+}
---
+++ TDBadgedCell (xcode project)/TDBadgedCell/TDBadgedCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TDBadgedCell
+//
+//  Created by Tim Davies on 07/09/2016.
+//  Copyright © 2016 Tim Davies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
