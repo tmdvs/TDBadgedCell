87c5fa15aa6bb3bc3af2fa8b4e1bcd739e0087ad
Guglielmo Faglioni <guglielmo@gorillatech.io>
2024-04-15 11:45:57 +0200

    Update Package.swift (#94)

--- Package.swift
+++ Package.swift
@@ -1,7 +1,24 @@
+// swift-tools-version:5.0
 import PackageDescription
 
-let package = Package(name: "TDBadgedCell")
-
-package.targets = [
-    Target(name: "TDBadgedCell", dependencies: [])
-]
+let package = Package(
+    name: "TDBadgedCell",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "TDBadgedCell",
+            targets: ["TDBadgedCell"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "TDBadgedCell",
+            dependencies: [],
+            path: "TDBadgedCell",
+            sources: ["TDBadgedCell.swift"]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
